a554c1301adf3b2947cf809483316279f62359e7
yingtingyang <yangy389@newschool.edu>
2016-10-13 08:45:38 -0400

    bear movement and animation state change

---
+++ Salmon Fight/Assets/movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 1.5f;
+
+    void Update()
+    {
+       
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
+        
+    
+
---
+++ Salmon Fight/Assets/scripts/animechange.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animechange : MonoBehaviour {
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("d"))
+        {
+            anim.Play("bear walk", -1, 0f);
+        }
+
+       
+        if(Input.GetMouseButtonDown(0))
+        {
+            anim.Play("bear attack", -1, 0f);
+        }
+       
+    }
+}
